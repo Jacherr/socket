f8feaa3a4432f4af54b3204a8a98e805ab3959a0
Jacher <jwc2002@outlook.com>
2020-08-11 10:39:28 +0100

    discourage use of base class, should be extended instead

--- src/socket.ts
+++ src/socket.ts
@@ -5,11 +5,6 @@ import { SocketEvents } from './constants'
 
 export interface SocketOptions extends ClientOptions {
     autoReconnect?: boolean
-    onClose?: (code: number, reason: string) => void | Promise<void>,
-    onError?: (err: Error) => void | Promise<void>,
-    onMessage?: (data: Data) => void | Promise<void>,
-    onOpen?: () => void | Promise<void>,
-    parseMessage?: <T> (data: Data) => T
 }
 
 export interface Socket {
@@ -69,30 +64,21 @@ export class Socket extends EventEmitter {
 
     public onClose(code: number, reason: string): void | Promise<void> {
         this.removeAllListeners();
-        this.options.onClose?.(code, reason);
     }
 
     public onError(err: Error) {
-        if(this.options.onError)  { 
-            this.options.onError(err);
-        } else {
-            throw err; // unhandled error event
-        }
+        throw err; // unhandled error event
     }
 
     public onMessage(data: Data) {
-        this.options.onMessage?.(data);
+
     }
 
     public onOpen(): void | Promise<void> {
-        this.options.onOpen?.()
+
     }
 
     public parseMessage<T>(data: Data): T | Data {
-        if(this.options.parseMessage) {
-            return this.options.parseMessage<T>(data);
-        } else {
-            return data;
-        }
+        return data;
     }
 }
